fc32c7ed68fbffa1402c7bd86ae1d5d15573ae42
loserq <2650009628@qq.com>
2021-01-17 21:06:16 +0800

    If: Use if... else to determine whether the input number is greater than one.

---
+++ judge/If/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+	int x;
+	int y =1;
+	printf("plz type a number:");
+	scanf_s("%d", &x);
+	if (x > y) {
+		printf("你输入了一个大于1的数");
+	}
+	else
+	{
+		printf("你输入了一个小于1的数");
+	}
+}
