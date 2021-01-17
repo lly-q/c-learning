92060356692dd25766eb2f1895846aebf9626910
loserq <2650009628@qq.com>
2021-01-17 20:11:40 +0800

    Ternary operator: Use the ternary operator to determine the positive and negative of the input number.

---
+++ judge/Ternary operator/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+	int num;
+	/*注意下面两行是相互联系的，printf中的冒号为输入的数字提供入口*/
+	printf("plz type a num:");
+	scanf_s("%d", &num);
+	/*用了三元运算符*/
+	(num > 0) ? printf("正数") : printf("负数或0");
+}
