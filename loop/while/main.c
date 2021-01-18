3f0f4290d17782e84a47a6e19e274cd7ee7aa906
loserq <2650009628@qq.com>
2021-01-18 14:00:53 +0800

    while: Loop execution using while.

---
+++ loop/while/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int effect = 1;
+	printf("冲他妈的\n");
+	while (effect < 11)
+	{
+		
+		printf("第%d次努力 \n",effect);
+		effect++;
+		
+	}
+	printf("达成成就：初出茅庐\n");
+	return 0;
+	/*十次努力就能初出茅庐*/
+}
