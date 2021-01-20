12b4c8f6df3fbe9f8d45028713a5f90f9be45dab
loserq <2650009628@qq.com>
2021-01-20 22:39:47 +0800

    pointers: Add *p usage.

--- pointers/main.c
+++ pointers/main.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main() {
 	int a = 1;
-	int p;
+	int p = 2;
+	int *b = p;
 	p = &a;
-	printf("a的地址%d，p的地址%d", p,&p);
+	printf("a的地址%d，\np的地址%d,\nb的值%d。\n", p,&p,b);
 	return 0;
 }
